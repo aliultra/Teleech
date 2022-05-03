1e89f8a2d4cf6949b4a33b8cc7c14cfff4e7379f
Alireza <61411316+aliultra@users.noreply.github.com>
2022-05-03 22:40:12 +0430

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,34 +1,22 @@
 class Translation(object):
-    START_TEXT = """Hi {},
-I'm X-URL-Uploader!
-You can upload HTTP/HTTPS direct link, Using this bot!
+    START_TEXT = """خوش آمدید {} !
 
-/help for more details!"""
-    FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
-    SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
+راهنمای استفاده از بات تلیچر /help"""
+    FORMAT_SELECTION = "در صورت درست بودن لینک، از دکمه زیر جهت شروع آپلود اقدام کنید."
+    SET_CUSTOM_USERNAME_PASSWORD = """در صورت پرایویت بودن لینک شما، لینکتان را با فرمت زیر ارسال کنید:
 URL | filename | username | password"""
-    DOWNLOAD_START = "Now Downloading.."
-    UPLOAD_START = "Now Uploading.."
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Thanks for using @xurluploaderbot)"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@xurluploaderbot"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
-    DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
+    DOWNLOAD_START = "در حال دانلود.."
+    UPLOAD_START = "در حال آپلود.."
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nFile Size: {}\nمتاسفانه! حجم فایل شما بیش از حد مجاز (2گیگابایت) است.."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "@teleech"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds.\n\n@teleech"
+    SAVED_CUSTOM_THUMB_NAIL = ""
+    DEL_ETED_CUSTOM_THUMB_NAIL = ""
     CUSTOM_CAPTION_UL_FILE = "{}"
-    NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
-    HELP_USER = """How to Use Me? Follow These steps!
-    
-1. Send url (example.domain/File.mp4 | New Filename.mp4).
-2. Send Image As Custom Thumbnail (Optional).
-3. Select the button.
-   SVideo - Give File as video with Screenshots
-   DFile  - Give File (video) as file with Screenshots
-   Video  - Give File as video without Screenshots
-   File   - Give File without Screenshots
-
-If bot didn't respond, contact @xgorn"""
-    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /genthumbnail to a media album, to generate custom thumbnail"
-    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
-    CANCEL_STR = "Process Cancelled"
+    NO_VOID_FORMAT_FOUND = ""
+    HELP_USER = """برای آپلود فایل مورد نظرتان کافیست لینک فایل را در قالب https و یا حتی http ارسال نمایید"""
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = ""
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = ""
+    CANCEL_STR = "لغو شد!"
     ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
-    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+    SLOW_URL_DECED = "سرور لینک شما بسیار کند است! دانلود فایل لغو شد."
